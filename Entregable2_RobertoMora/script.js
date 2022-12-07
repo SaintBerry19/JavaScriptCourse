9384128161162a90bd67c06c9daff6c3838d3b61
SaintBerry <robertomorab.19@gmail.com>
2022-12-07 15:07:49 -0600

    Filter

--- Entregable2_RobertoMora/script.js
+++ Entregable2_RobertoMora/script.js
@@ -7,41 +7,68 @@
 // múltiplos de 400 sí. Estos son algunos ejemplos de posibles respuestas: 2012 es bisiesto, 2010 no es
 // bisiesto, 2000 es bisiesto, 1900 no es bisiesto.
 
-function bisiesto(){    
-    let a=0
-    let r=0
-    let record=[]
-    while (a<1){
-        try{ 
-            let fecha = Number(prompt("Ingrese un año para consultar si es bisiesto o no: "))
-            let year= (fecha)
-            if ((year%4===0 && year%100!==0)||(year%400===0)){
-                record.push({'Año': year, 'Bisiesto':'Sí'})
-                alert(`El año ${year} es bisiesto`)
-            }    
-            else{
-                alert(`El año ${year} no es bisiesto`)
-                record.push({'Año': year, 'Bisiesto': 'No'})
-            } 
-            r=0
-            while (r<1){ 
-            b=(prompt("Desea hacer otra consulta: "))
-             if (b==="si" || b==="SI" || b==="Si" || b==="sI"){
-                r=+1
-                alert("Introduzca otro valor")
-            }
-             else if(b=="no" || b=="NO" || b=="No" || b=="nO"){              
-                r=+1
-                a=+1
-                alert("La consulta ha terminado")
-            }
-             else{
-                alert("Introduzca si o no")
-             }
-              }}
-        catch (error) {alert(error)}
+function bisiesto() {
+  let a = 0;
+  let r = 0;
+  let record = [];
+  let bisiesto = [];
+  let x = 0;
+  while (a < 1) {
+    try {
+      let fecha = Number(
+        prompt("Ingrese un año para consultar si es bisiesto o no: ")
+      );
+      let year = fecha;
+      if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        record.push({ year: year, bisiesto: "Si" });
+        alert(`El año ${year} es bisiesto`);
+      } else {
+        alert(`El año ${year} no es bisiesto`);
+        record.push({ year: year, bisiesto: "No" });
+      }
+      r = 0;
+      while (r < 1) {
+        b = prompt("Desea hacer otra consulta: ");
+        if (b === "si" || b === "SI" || b === "Si" || b === "sI") {
+          r = +1;
+          alert("Introduzca otro valor");
+        } else if (b == "no" || b == "NO" || b == "No" || b == "nO") {
+          r = +1;
+          a = +1;
+        } else {
+          alert("Introduzca si o no");
         }
-        console.log(record)
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }
+  try {
+    while (x < 1) {    
+        let mostrar = prompt(
+      "Escribe 'si' para mostrar los años bisiestos ingresados o 'no' para mostrar los no bisiestos:"
+    );
+      if (mostrar === "si") {
+        result = record.filter(({ bisiesto }) => bisiesto === "Si");
+        result.forEach((element) => {
+          alert(element.year);
+        });
+        console.log(result);
+        break
+      }
+      if (mostrar === "no") {
+        result = record.filter(({ bisiesto }) => bisiesto === "No");
+        result.forEach((element) => {
+          alert(element.year);
+        });
+        break
+      } else {
+        alert("Introduzca si o no");
+      }
+    }
+    alert ('El programa a finalizado')
+  } catch (error) {
+    alert(error);
+  }
 }
-bisiesto()
-
+bisiesto();
